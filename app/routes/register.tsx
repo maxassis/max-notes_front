2589d83d9d9e39d327c087458eb1e82e0024db19
max <max.assis@chatguru.com.br>
2023-07-17 19:03:26 -0300

    feat: fazendo tela de login

--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -8,11 +8,13 @@ export default function Login() {
        return (
     <div className="login">
         <div className="login__wrapper">
-            <h2 className="login__title">Login</h2>
+            <h2 className="login__title">Registro</h2>
             <h3 className="login__subtitle">Bem vindo</h3>
 
             <input className="login__input-user" type="text" placeholder="Username" />
             <input className="login__input-email" type="password" placeholder="Email" />
+            <input className="login__input-password" type="Password" placeholder="Senha" />
+            <input className="login__input-password" type="password" placeholder="Confirmar Senha" />
 
             <button className="login__button">Continue</button>
 
