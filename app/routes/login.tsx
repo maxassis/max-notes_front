02b4b33a636149aa1ece38aa2ae2716917187234
max <max.assis@chatguru.com.br>
2023-07-18 12:53:44 -0300

    feat: fazendo requisição da tela de login

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,8 +1,46 @@
 import styles from "../styles/login.css" ;
+import { useActionData, Form, Link } from "@remix-run/react";
+import { ActionArgs, redirect } from "@remix-run/node";
+import { z } from "zod";
+
 
 export function links() {
     return [{ rel: "stylesheet", href: styles }];
   }
+
+const schema = z.object({
+  email: z.string().email({message: "fornecer email valido"}),
+  password: z.string().min(6, {message: "minimo de 6 caracteres"}),
+});
+
+
+export async function action({request}: ActionArgs) {
+    const data = Object.fromEntries(await request.formData())
+    console.log(data);
+    
+    if(!schema.safeParse(data).success) {
+        console.log("deu ruim");
+        
+        return schema.safeParse(data);
+      }
+    
+      if(schema.safeParse(data).success) {
+        fetch("http://localhost:3333/auth/login", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            "email": data.email,
+            "password": data.password
+        })
+        }).then(response => response.json())
+        .then(json => console.log(json))
+        } 
+    
+    
+     return null
+    
+    }
+
 
 export default function Login() {
        return (
@@ -11,12 +49,16 @@ export default function Login() {
             <h2 className="login__title">Login</h2>
             <h3 className="login__subtitle">Bem vindo</h3>
 
-            <input className="login__input-user" type="text" placeholder="Username" />
-            <input className="login__input-email" type="password" placeholder="Email" />
+            <Form method="POST">
+                <input className="login__input-user" placeholder="Email" id="user" name="email" />
+                <input className="login__input-email" type="password" placeholder="Password" id="pass" name="password" />
 
-            <button className="login__button">Continue</button>
+                <button className="login__button">Continue</button>
+            </Form>
 
-            <h4 className="login__forgot">Novo usuário ? <span>Registre-se</span></h4>
+            <Link to="/register">
+                <h4 className="login__forgot">Novo usuário ? <span>Registre-se</span></h4>
+            </Link>
         </div>
     </div>
     )
--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -1,8 +1,11 @@
-import styles from "../styles/login.css" ;
+import styles from "../styles/login.css";
+import { Link } from "@remix-run/react"
 
 export function links() {
     return [{ rel: "stylesheet", href: styles }];
   }
+
+
 
 export default function Login() {
        return (
@@ -12,13 +15,20 @@ export default function Login() {
             <h3 className="login__subtitle">Bem vindo</h3>
 
             <input className="login__input-user" type="text" placeholder="Username" />
-            <input className="login__input-email" type="password" placeholder="Email" />
-            <input className="login__input-password" type="Password" placeholder="Senha" />
+            <input className="login__input-email" type="email" placeholder="Email" />
+            <input className="login__input-password" type="password" placeholder="Senha" />
             <input className="login__input-password" type="password" placeholder="Confirmar Senha" />
 
-            <button className="login__button">Continue</button>
+            <div className="login__checkbox">
+                <input type="checkbox" />
+                <span>Aceito os termos e condições</span>
+            </div>
 
-            <h4 className="login__forgot">Novo usuário ? <span>Registre-se</span></h4>
+            <button className="login__button" style={{marginTop: "1.84rem"}}>Continue</button>
+
+            <Link to="/login">  
+                <h4 className="login__forgot">Já tem uma conta ? <span>Login</span></h4>
+            </Link>  
         </div>
     </div>
     )
