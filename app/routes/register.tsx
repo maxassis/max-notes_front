e730311e7b9d8dd855fc6175793736fefdabcc59
max <max.assis@chatguru.com.br>
2023-07-26 17:27:07 -0300

    feat: feito mensagem de erro na requisição

--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -18,15 +18,15 @@ const schema = z.object({
 
   export async function action({ request }: ActionArgs) {
     const data = Object.fromEntries(await request.formData());
-    console.log(data);
+    //console.log(data);
   
     if (!schema.safeParse(data).success) {
-      console.log("deu ruim");
+     // console.log("deu ruim");
    
       return schema.safeParse(data);
     }
    
-    console.log("deu bom");
+   // console.log("deu bom");
     const token = fetch("http://localhost:3333/auth/register", {  
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -37,17 +37,84 @@ const schema = z.object({
       }),
     })
       .then((r) => r.json())
-      .then((json) => json.accesstoken);
+      .then((json) => {
+        if(json.error) throw new Error("Este email ja está cadastrado");
+        return json.accesstoken
+      })
    
     const session = await getSession(request.headers.get("Cookie"));
     session.set("token", await token);
     //console.log(session.data);
    
-    return redirect("/notes", {
+    return redirect("/notes/show", {
       headers: {
         "Set-Cookie": await commitSession(session),
       },
     });
+  }
+
+
+  export function ErrorBoundary() {
+    const data = useActionData();
+   // console.log(data)
+    
+    return (
+      <div className="login">
+      <div className="login__wrapper">
+        <h2 className="login__title">Registro</h2>
+        <h3 className="login__subtitle2">Bem vindo</h3>
+
+        <Form method="POST">
+          <div className="login__input-wrapper">
+          <span className="login__error-message">Este email ja está cadastrado</span>
+            <input
+              className="login__input-user input"
+              type="text"
+              placeholder="Username"
+              name="name"
+            />
+            {data?.error?.issues.some((item: any) => item.path.includes("name")) ? <span className="login__error">Nome e obrigatorio</span> : null}
+          </div>
+
+          <div className="login__input-wrapper">
+            <input
+              className="login__input-email input"
+              placeholder="Email"
+              name="email"
+            />
+            {data?.error?.issues.some((item: any) => item.path.includes("email")) ? <span className="login__error">Insira um email valido</span> : null}
+          </div>
+
+          <div className="login__input-wrapper">
+            <input
+              className="login__input-password input"
+              type="password"
+              name="password"
+              placeholder="Senha"
+            />
+            {data?.error?.issues.some((item: any) => item.path.includes("password")) ? <span className="login__error">A senha de possuir no minimo 6 letras</span> : null}
+          </div>
+
+          <div className="login__checkbox">
+            <input type="checkbox" name="check" value="check" />
+            <span>Aceito os termos e condições</span>
+            {/* {data?.error?.issues.some((item: any) => item.path.includes("check")) ? <span className="login__terms" >Marque a caixa</span> : null} */}
+          </div>
+          {data?.error?.issues.some((item: any) => item.path.includes("check")) ? <span className="login__terms" >Aceite os termos</span> : null}
+
+          <button className="login__button" style={{ marginTop: "1.84rem" }}>
+            Continue
+          </button>
+        </Form>
+
+        <Link to="/login">
+          <h4 className="login__forgot">
+            Já tem uma conta ? <span>Login</span>
+          </h4>
+        </Link>
+      </div>
+    </div>
+    );
   }
 
 
