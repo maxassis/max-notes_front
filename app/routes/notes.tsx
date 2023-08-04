51274e383c8e326f80b26b13181317c863659351
max <max.assis@chatguru.com.br>
2023-08-04 16:12:21 -0300

    estilizando a barra lateral

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -51,15 +51,22 @@ export async function action({ request }: ActionArgs) {
 
 export default function Notes() {
   const [input, setInput] = useState<string>("")
+  const [menu, setMenu] = useState<boolean>(false)
   const data = useActionData()
   
   return (
     <div className="notes">
       <header className="notes__header">
       <div className="header">
-        <svg className="header__icon" xmlns="http://www.w3.org/2000/svg" width="30" height="30">
+        {!menu ? (
+        <svg onClick={() => setMenu(!menu)} className="header__icon" xmlns="http://www.w3.org/2000/svg" width="30" height="30">
             <path d="M3 7a1 1 0 1 0 0 2h24a1 1 0 1 0 0-2H3zm0 7a1 1 0 1 0 0 2h24a1 1 0 1 0 0-2H3zm0 7a1 1 0 1 0 0 2h24a1 1 0 1 0 0-2H3z"/>
         </svg>
+        ) : (
+        <svg className="header__icon" onClick={() => setMenu(!menu)} xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24">
+            <path fill="none" stroke="currentColor" strokeWidth="2" d="m3 3 18 18M3 21 21 3"/>
+        </svg>
+        )}
         <Form method="POST" className="header__form"> 
           <div style={{position: "relative"}}> 
             <input value={input} onChange={(e) => setInput(e.target.value)} className="header__input" placeholder="Pesquisar" name="content" />
@@ -82,6 +89,9 @@ export default function Notes() {
       </aside>
 
       <main className="notes__main">
+        <div className={"menu-mobile__wrapper " + (menu ? "menu-mobile--show-wrapper"  : "")} onClick={() => setMenu(false)}>
+          <h1 className={"menu-mobile " + (menu ? "menu-mobile--show"  : "")}></h1>
+        </div>
         <Outlet />
       </main>
     </div>
